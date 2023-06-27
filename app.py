8fc938360f13bdeb0865ae52cd685775c81232aa
Yuuma <y.sato.sys22@morijyobi.ac.jp>
2023-06-27 11:05:49 +0900

    ログイン機能の追加

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import db, random, string
+
+app = Flask(__name__)
+app.secret_key = ''.join(random.choices(string.ascii_letters, k = 256))
+
+@app.route('/', methods=['GET'])
+def index():
+    msg = request.args.get('msg')
+    if msg == None:
+        return render_template('index.html')
+    else:
+        return render_template('index,html', msg=msg)
+    
+
+app.route('/', methods=['POST'])
+def login():
+    user_name = request.form.get('username')
+    password = request.form.get('password')
+    if db.login(user_name, password):
+        session['user'] = True
+    else:
+        error = 'ログインに失敗しました'
+        input_date = {'user_name':user_name, 'password':password}
+        return render_template('index.html', error=error)
+
+app.route('/admin', methods=['GET'])
+def admin():
+    if 'user' in session:
+        return render_template('admin.html')
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/register')
+def register_form():
+    return render_template('register.html')
+
+@app.route('/register.exe')
+def register_exe():
+    user_name=request.form.get('username')
+    password=request.form.get('password')
+    mail= request.form.get('mail')
+    
+    if user_name == '':
+        error = 'ユーザー名が未入力です'
+        return render_template('register.html', error=error)
+    if password == '':
+        error = 'パスワードが未入力です'
+        return render_template('register.html', error=error)
+    
+    count = db.insert_user(user_name, mail, password)
+    if count == 1:
+        msg='登録が完了しました'
+        return redirect(url_for('index', msg=msg))
+    else:
+        error='登録に失敗しました'
+        return render_template('register.html', error=error)
+    
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+        

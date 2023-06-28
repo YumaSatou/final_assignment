a2c03bb9b66bf4d057c087d0a6cb0cd7104f6676
Yuuma <y.sato.sys22@morijyobi.ac.jp>
2023-06-28 13:54:10 +0900

    テーブルを新しく追加しました

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -4,3 +4,10 @@ CREATE TABLE admin_account(
     hashed_password VARCHAR(64),
     salt VARCHAR(30)
 );
+
+CREATE TABLE books_list(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(64),
+    author VARCHAR(64),
+    publlisher VARCHAR(64)
+);
